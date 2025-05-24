c5461f6c110497f4ae48da2c5f62141f4255e80b
Dhruv280704 <Dhruvking425@gmail.com>
2025-05-24 12:15:12 +0530

    first commit

---
+++ Frontend/src/components/About.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="about container">
+      <h4>Events</h4>
+      <h2>ABOUT</h2>
+      <p>
+        Welcome to **Prestige Planner**, where your dream events come to life with elegance and precision. Whether it’s a grand wedding, a corporate affair, or an intimate celebration, we specialize in creating unforgettable experiences tailored to your vision. With expert planning, creative designs, and flawless execution, we ensure every detail is perfect. At Prestige Planner, we turn your special moments into lifelong memories. Trust us to handle everything, so you can enjoy every second of your celebration stress-free.
+
+      </p>
+    </section>
+  );
+};
+
+export default About;
